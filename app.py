fb9b5bb359d6b4e0942cbc7a8d7684d140647afd
IsamiRi <53663257+IsamiRi@users.noreply.github.com>
2023-02-17 09:48:30 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import markdown
+import os
+
+app = Flask(__name__, template_folder='')
+
+@app.route('/<page_name>')
+def show_page(page_name):
+    # Look for a Markdown file with the same name as the requested page
+    md_file_path = f"pages/{page_name}.md"
+    if not os.path.isfile(md_file_path):
+        return render_template('404.html'), 404
+
+    # Read the Markdown file and convert it to HTML
+    with open(md_file_path, 'r', encoding='utf-8') as f:
+        md_text = f.read()
+    html = markdown.markdown(md_text, output_format='html5', extensions=['fenced_code'])
+
+    # Render the HTML as a template
+    return render_template('page.html', html=html, page_name=page_name)
+
+@app.route('/')
+def main_page():
+    # Look for a Markdown file with the same name as the requested page
+    md_file_path = f"pages/index.md"
+    if not os.path.isfile(md_file_path):
+        return render_template('404.html'), 404
+
+    # Read the Markdown file and convert it to HTML
+    with open(md_file_path, 'r', encoding='utf-8') as f:
+        md_text = f.read()
+    html = markdown.markdown(md_text, output_format='html5', extensions=['fenced_code'])
+
+    # Render the HTML as a template
+    return render_template('page.html', html=html, page_name=page_name)
